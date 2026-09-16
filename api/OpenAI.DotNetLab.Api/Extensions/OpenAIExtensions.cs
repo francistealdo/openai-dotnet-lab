6048d16a468454aca296597719f7c6f9e67b182c
Francis Tealdo <francistealdo@gmail.com>
2026-09-15 23:53:39 -0300

    feat: add initial .NET API setup for OpenAI integration

---
+++ api/OpenAI.DotNetLab.Api/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace OpenAI.DotNetLab.Api.Extensions
+{
+    public static class OpenAIExtensions
+    {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            var apiKey = builder.Configuration["OpenAI:ApiKey"];
+            var apiKeySecret = Environment.GetEnvironmentVariable(apiKey);
+
+            if (string.IsNullOrEmpty(apiKeySecret))
+            {
+                throw new InvalidOperationException("OpenAI API key is not set in the environment variables.");
+            }
+            ;
+
+            var openAIClient = new OpenAIClient(apiKeySecret);
+            builder.Services.AddSingleton(openAIClient);
+
+            return builder;
+        }
+    }
+}
---
+++ api/OpenAI.DotNetLab.Api/Program.cs
@@ -0,0 +1,26 @@
+using OpenAI.DotNetLab.Api.Extensions;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddOpenAI();
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
